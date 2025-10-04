b4211df5beb25bdf025857c633f783b3554a8e7f
AdityaInnovator <adityaprasath.s27@gmail.com>
2025-10-04 02:22:17 -0500

    Update start-worker.sh

--- start-worker.sh
+++ start-worker.sh
@@ -10,6 +10,16 @@ ssh-add ~/.ssh/shared_rsa
 # DO NOT MODIFY THE ABOVE ##########################################################
 ####################################################################################
 
+export JAVA_HOME=/usr/local/openjdk-8
+export SPARK_HOME=/opt/spark
+export PATH=$SPARK_HOME/bin:$SPARK_HOME/sbin:$PATH
+
+$SPARK_HOME/sbin/stop-worker.sh || true
+pkill -f org.apache.spark.deploy.worker.Worker || true
+rm -rf /opt/spark/work/* /tmp/spark* /var/tmp/spark* 2>/dev/null || true
+
+/opt/spark/sbin/start-worker.sh --host "$(hostname)" spark://main:7077
+
 export SPARK_LOCAL_HOSTNAME="$(hostname)"
 export SPARK_LOCAL_IP=$(getent hosts "$(hostname)" | awk '{print $1}')
 
