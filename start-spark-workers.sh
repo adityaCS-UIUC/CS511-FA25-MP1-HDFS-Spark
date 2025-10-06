3a7981258ed6985a4608571aec251dc0e599e3cf
AdityaInnovator <adityaprasath.s27@gmail.com>
2025-10-06 10:17:48 -0500

    Update start-spark-workers.sh

--- start-spark-workers.sh
+++ start-spark-workers.sh
@@ -9,25 +9,14 @@ ssh-add ~/.ssh/shared_rsa
 # DO NOT MODIFY THE ABOVE ##########################################################
 ####################################################################################
 
-echo "Starting Spark worker on worker1..."
-docker compose -f cs511p1-compose.yaml exec worker1 bash -c 'export SPARK_HOME=/opt/spark && export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin && export JAVA_HOME=/usr/local/openjdk-8/jre && $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port 8081 spark://main:7077 &'
+# Start HDFS/Spark worker here
+export JAVA_HOME="/usr/local/openjdk-8/jre"
 
-echo "Starting Spark worker on worker2..."
-docker compose -f cs511p1-compose.yaml exec worker2 bash -c 'export SPARK_HOME=/opt/spark && export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin && export JAVA_HOME=/usr/local/openjdk-8/jre && $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port 8081 spark://main:7077 &'
+export HDFS_DATANODE_USER="root"
+# bash
+echo "Starting DataNode..."
+# Start the DataNode service
+$HADOOP_HOME/sbin/hadoop-daemon.sh start datanode
 
-echo "Waiting for workers to start..."
-sleep 5
-
-echo "Checking Spark processes..."
-echo "=== Main ==="
-docker compose -f cs511p1-compose.yaml exec main ps aux | grep spark
-
-echo "=== Worker1 ==="
-docker compose -f cs511p1-compose.yaml exec worker1 ps aux | grep spark
-
-echo "=== Worker2 ==="
-docker compose -f cs511p1-compose.yaml exec worker2 ps aux | grep spark
-
-echo "Testing Spark Q1..."
-docker compose -f cs511p1-compose.yaml cp resources/active_executors.scala main:/active_executors.scala
-docker compose -f cs511p1-compose.yaml exec main bash -c 'export SPARK_HOME=/opt/spark && export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin && cat /active_executors.scala | spark-shell --master spark://main:7077' | grep "res0:"
+# Keep the container running
+tail -f /dev/null
