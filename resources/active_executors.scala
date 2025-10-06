3d049a5c71bd0f508fd64f84832b4c76d6f95569
AdityaInnovator <adityaprasath.s27@gmail.com>
2025-10-06 09:46:42 -0500

    Update active_executors.scala

--- resources/active_executors.scala
+++ resources/active_executors.scala
@@ -1,25 +1,9 @@
-import java.net.InetAddress
 import org.apache.spark.SparkContext
-import scala.util.Try
 
 def currentActiveExecutors(sc: SparkContext): Seq[String] = {
-  sc.parallelize(1 to 1000, 3).count()
-  val endpoints = sc.getExecutorMemoryStatus.keys.toSeq
-  val hosts     = endpoints.map(_.split(":")(0)).distinct
-  val knownNames = Array("main","worker1","worker2")
-  val nameToIp   = knownNames.flatMap(n => Try(InetAddress.getByName(n).getHostAddress).toOption.map(ip => n -> ip)).toMap
-  val ipToName   = nameToIp.map(_.swap)
-  val normalized = hosts.map(h => ipToName.getOrElse(h, h)).distinct
-  val rawDriver  = Try(sc.getConf.get("spark.driver.host")).toOption.getOrElse("main")
-  val driver     = ipToName.getOrElse(rawDriver, rawDriver)
-  normalized.filterNot(_ == driver).sorted
+ val allExecutors = sc.getExecutorMemoryStatus.map(_._1)
+ val driverHost: String = sc.getConf.get("spark.driver.host")
+ allExecutors.filter(! _.split(":")(0).equals(driverHost)).toList
 }
 
-// Force execution even when file is piped via stdin
-try {
-  currentActiveExecutors(sc).foreach(println)
-} catch {
-  case _: Throwable => // sc not ready yet; spin once and retry
-    sc.parallelize(1 to 1, 1).count()
-    currentActiveExecutors(sc).foreach(println)
-}
+currentActiveExecutors(sc)
